54f192d5d4a0ffef79d4a4cc313a56e9bdd97a36
tsubochi1 <tsubochi1@yahoo.co.jp>
2020-07-18 05:08:57 +0000

    finish_C8

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,8 +1,11 @@
 class TasksController < ApplicationController
+  
   before_action :set_user
+  
   def new
     @task = Task.new
   end
+  
   def create
     @task = @user.tasks.new(task_params)
    if @task.save
@@ -21,6 +24,21 @@ class TasksController < ApplicationController
     @task = Task.find(params[:id])
   end
   
+  def edit
+    @task = Task.find(params[:id])
+  end
+  
+  
+  def update
+    @task = Task.find(params[:id])
+    if @task.update_attributes(task_params)
+      flash[:success] = "タスクを更新しました。"
+      redirect_to user_task_path
+    else
+      render :edit      
+    end
+  end
+
   def destroy
     @task = Task.find(params[:id])
     @task.destroy
@@ -30,11 +48,11 @@ class TasksController < ApplicationController
 
   private
   def set_user
-    @user = User.find(params[:user_id])
+    @user = User.find(session[:user_id])
   end
   
   def task_params
-    params.require(:task).permit(:name,:description,)
+    params.require(:task).permit(:name, :description)
   end
-  
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   
   root 'static_pages#top'
   get '/signup', to: 'users#new'
-  get '/users', to: 'users#index'
   get    '/login', to: 'sessions#new'
   post   '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
