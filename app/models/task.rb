8b57f666d45951d59b1cdb3ccec7cd6dff065a4c
tsubochi1 <tsubochi1@yahoo.co.jp>
2020-08-05 11:55:23 +0000

    finish_C12

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,5 +1,5 @@
 class Task < ApplicationRecord
   belongs_to :user
   validates :name, presence: true, length: { maximum: 50 }
-  validates :description, presence: true, length: { maximum: 140 }
+  validates :description, presence: true, length: { minimum: 5 }
 end
